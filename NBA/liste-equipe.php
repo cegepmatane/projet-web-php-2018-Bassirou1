33470d829845dae3a1ecdf065dd73c6f1598aae3
Bassirou <haidara.bassirou@yahoo.com>
2019-03-23 09:59:56 -0400

    Maintance de l'ajax dans ma barre de recherche closes #2

--- NBA/liste-equipe.php
+++ NBA/liste-equipe.php
@@ -1,26 +1,26 @@
-<?php 
+<?php
 
 		include "accesseur/EquipeDAO.php";
-	
+
 	$equipeDao = new EquipeDAO();
 	$recherche = "";
 	if(!empty($_POST['action-rechercher']))
 	{
 		//print_r($_POST);
-		$recherche = filter_var($_POST['recherche'],FILTER_SANITIZE_STRING); 
+		$recherche = filter_var($_POST['recherche'],FILTER_SANITIZE_STRING);
 		//echo $recherche;
 		$listeEquipe = $equipeDao->rechercherEquipe($recherche);
-			
+
 	}
 	else
 	{
 		$listeEquipe = $equipeDao->lireListe();
 		//print_r($listeEquipe);
 	}
-		
 
-	
-	
+
+
+
 ?>
 <!doctype html>
 <html lang="fr">
@@ -36,34 +36,34 @@
 			recherche = document.querySelector("#recherche").value;
 			parametre = "recherche=" + recherche;
 			console.log();
-			
+
 			// ETAPE 2 - REQUETE
 			ajax = new Ajax();
 			//console.log(ajax);
-			url = 'http://142.44.162.203/nba/action/suggestion.php';
+			url = 'http://localhost/projet-web-php-2018-Bassirou1/nba/action/suggestion.php';
 			ajax.executer("GET", url, parametre, recevoirLesSuggestions);
-			
-			
+
+
 		}
-		
+
 		// ETAPE 3 - RECEPTION (traitement)
 		function recevoirLesSuggestions(ajax)
 		{
 			suggestions = ajax.responseText;
 			console.log('suggestions='+suggestions);
-			
+
 			// ETAPE 4 - AFFICHAGE (retroaction)
 			document.querySelector("#boite-suggestions").style.display = "block";
 			document.querySelector("#boite-suggestions").innerHTML = suggestions;
-			
+
 		}
 		// ETAPE 5 - SUITE
 		function afficherSuggestion(suggestion)
 		{
 			document.querySelector('#recherche').value=suggestion;
-			document.querySelector("#boite-suggestions").style.display = "none";			
+			document.querySelector("#boite-suggestions").style.display = "none";
 		}
-		
+
 	</script>
 	<style>
 	#section-recherche
@@ -84,22 +84,22 @@
 		<h1><?=_('NBA')?></h1>
 		<nav></nav>
 	</header>
-	
+
 	<section id="contenu">
 		<header><h2><?=_('Liste des equipes')?></h2></header>
-	
+
 	<section id="section-recherche">
 		<form method="post" action="" id="formulaire-recherche">
-			
+
 			<input type="text" name="recherche" id="recherche" value="<?=$recherche?>" onkeyup="rechercherLesSuggestions()"/>
 			<input type="submit" value="Rechercher"  name="action-rechercher"/>
-		
+
 		</form>
 		<div id="boite-suggestions"></div>
-	
-	
+
+
 	</section>
-	
+
 		<?php
 		foreach($listeEquipe as $equipe)
 		{
@@ -108,18 +108,18 @@
 			<div>
 				<a href="equipe.php?equipe=<?=$equipe->idEquipe?>">
 					<?=$equipe->nom?>
-				
+
 				</a>
-				
+
 			</div>
 		</div>
 		<?php
 		}
 		?>
-	<a href="inscription.php"><?=_('Sinscrire')?></a> 
+	<a href="inscription.php"><?=_('Sinscrire')?></a>
 	<a href="connexion.php"><?=_('Me connecter')?></a>
 	</section>
-	
+
 	<footer><span id="signature"></span></footer>
 </body>
 </html>
